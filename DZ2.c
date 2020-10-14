4eb505d465d64bdd3fdc552e8f7b9055be09ebb4
ShadrinYuri <53979055+ShadrinYuri@users.noreply.github.com>
2020-10-15 00:09:24 +0300

    Update DZ2.c

--- DZ2.c
+++ DZ2.c
@@ -35,11 +35,6 @@ int main(void)
         name[i] = sname;
     }
 
-    printf("\n");
-
-
-//    a-b;a-c;b-c;
-//
     printf("Enter connection\n");
     char *str = (char*) malloc(100 * sizeof(char));
     scanf("%s", str);
@@ -52,18 +47,8 @@ int main(void)
         char *second_name;
         int index_second_name = -1;
 
-        //if (k != 0)
-            //strcpy(first_name, strtok(NULL, "-"));
-        //else
-            //strcpy(first_name, strtok(str, "-"));
         second_name = strtok(NULL, ";");
-        // if (first_name == NULL)
-        //     break;
-        printf("%s\n", first_name);
-        printf("%s\n", second_name);
-        
-        //k += strlen(first_name) + strlen(first_name) + 2;
-        //printf("%d\n", k);
+
         for( int i = 0; i < n; i++)
         {
             if(strcmp(name[i], first_name) == 0)
@@ -72,8 +57,6 @@ int main(void)
                 index_second_name = i;
         }
 
-        printf("%d\n", index_first_name);
-        printf("%d\n", index_second_name);
         if ((index_first_name != -1) && (index_second_name != -1))
         {
             if (fl == 1)
@@ -86,53 +69,53 @@ int main(void)
                 graph[index_first_name][index_second_name] = 1;
             }
         }
-        printf("sdsds\n");
-        //free(first_name);
-        //free(second_name);
-        //first_name = (char*) malloc(20 * sizeof(char));
+
         first_name = strtok(NULL, "-");
-        printf("%s\n", first_name);
     }
-    printf("and\n");
-//
-    _Bool goriz = 1;
-    for (int i = 0; i < n; i++){
-        _Bool vertic = 0;
-        for (int j = 0; j < n; j++){
+
+    int side = 1;
+    for (int i = 0; i < n; i++)
+    {
+        int up = 0;
+        for (int j = 0; j < n; j++)
+        {
             if (graph[i][j] > 0)
-                vertic = 1;
+                up = 1;
 
             if (graph[j][i] > 0)
-                vertic = 1;
+                up = 1;
         }
-        if (vertic == 0)
-            goriz = 0;
+        if (up == 0)
+            side = 0;
     }
-    if (!goriz){
+    if (!side)
         printf("Unrelated graph\n");
-    }
-    else {
+    else
         printf("Related graph\n");
-    }
 
-    for (int i = 0; i < n; i++){
-        printf("#%d %10s: ", i + 1, name[i]);
-            for (int k = 0; k < n; k++){
-                printf("%d ", graph[i][k]);
-            }
-            printf("\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("#%4s: ", name[i]);
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d ", graph[i][j]);
+        }
+        printf("\n");
     }
 
 
 
     char* arr = (char*) calloc(500, sizeof(char));
 
-    if(fl == 2){
+    if(fl == 2)
+    {
         strcat(arr, "digraph G {");
-        for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
-            if (graph[i][j] > 0){
-                for (int a = 0; a < graph[i][j]; a++){
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (graph[i][j] > 0)
+                {
                     strcat(arr, name[i]);
                     strcat(arr, "->");
                     strcat(arr, name[j]);
@@ -141,42 +124,35 @@ int main(void)
             }
         }
     }
-    }
-    else {
+    else
+    {
         strcat(arr, "graph G {");
-        for (int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++)
+        {
             strcat(arr, name[i]);
             strcat(arr, ";");
         }
-        for (int i = 0; i < n; i++){
-            for (int j = i; j < n; j++){
-                if (graph[i][j] > 0){
-                    for (int a = 0; a < graph[i][j]; a++){
-                        strcat(arr, name[i]);
-                        strcat(arr, "--");
-                        strcat(arr, name[j]);
-                        strcat(arr, ";");
-                    }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i; j < n; j++)
+            {
+                if (graph[i][j] > 0)
+                {
+                    strcat(arr, name[i]);
+                    strcat(arr, "--");
+                    strcat(arr, name[j]);
+                    strcat(arr, ";");
                 }
             }
         }
-        }
+    }
 
     strcat(arr, "}");
 
-    FILE* f = fopen("/Users/aliagumirova/Desktop/Yuri/graph.dot", "w");
+    FILE* f = fopen("graph.dot", "w");
     fprintf(f, "%s\n", arr);
     fclose(f);
 
-    char* term = (char*) calloc(500, sizeof(char));
-
-    strcat(term, "echo \"");
-    strcat(term, arr);
-
-    strcat(term, "\" | dot -Tpng >./graph_pikcha.png");
-    printf("%s\n", term);
-    system(term);
-    free(term);
     free(arr);
 
     return 0;
